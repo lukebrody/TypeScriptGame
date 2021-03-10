dddbbb4333deddc02f89bbaa40df1ccb01e2e039
Luke Brody <lukebrody@pavonine.co>
2021-03-09 17:48:11 -0800

    Simple server implementation
    Client sends data, sends to other clients

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,27 +2,33 @@ import * as express from 'express';
 import * as http from 'http';
 import * as WebSocket from 'ws';
 import { AddressInfo } from 'net';
-import Timer = NodeJS.Timer;
 
 const app = express();
 
 app.use(express.static("../client/build"))
 
-// initialize a simple http server
 const server = http.createServer(app);
 
-// initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
+let sockets: Set<WebSocket> = new Set<WebSocket>();
+
 wss.on('connection', (ws: WebSocket) => {
-    console.log('connection recieved');
+    ws.onopen = event => {
+        sockets.add(ws);
+    };
+    ws.close = event => {
+        sockets.delete(ws);
+    }
     ws.onmessage = event => {
-        console.log('returning data');
-        ws.send(event.data);
+        sockets.forEach(otherSocket => {
+            if(otherSocket != ws) {
+                otherSocket.send(event.data);
+            }
+        });
     };
 });
 
-// start our server
 server.listen(process.env.PORT || 8080, () => {
     console.log(`Server started on port ${(server.address() as AddressInfo).port} :)`);
 });
